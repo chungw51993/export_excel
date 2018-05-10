29fb8d693d2cdc2ff6b229ea50590119d2978b23
Won Jin Chung <chungw51993@gmail.com>
2018-05-09 20:54:28 -0500

    Downloading with right data

--- client/components/Export.jsx
+++ client/components/Export.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import ReactExport from 'react-data-export';
 
+const ExcelFile = ReactExport.ExcelFile;
+const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
+const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
 import Row from './Row.jsx';
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -18,6 +22,7 @@ class Export extends Component {
       days: 0,
       month: '',
       dayArr: [],
+      showDownload: false,
     };
   }
 
@@ -57,7 +62,7 @@ class Export extends Component {
       const obj = {
         day: i,
         total: '',
-        gas: '',
+        gas: 0,
         lotto: '',
         inside: '',
         beer: '',
@@ -83,6 +88,7 @@ class Export extends Component {
     const inside = parseInt(dayArr[index].beer) + parseInt(dayArr[index].cigarette);
     const decimals = (Math.random() * 99).toFixed(2).toString().split('.')[1];
     const lotto50 = Math.random() >= 0.7;
+    dayArr[index].inside = inside;
     dayArr[index].insideWDecimal = (dayArr[index].cigarette > 0 && dayArr[index].beer > 0) ? (inside).toString().concat('.', decimals) : 0;
     dayArr[index].lotto50 = (lotto50 && dayArr[index].lotto > 0) ? (dayArr[index].lotto).toString().concat('.50') : (dayArr[index].lotto).toString().concat('.00');
     this.setState({
@@ -90,12 +96,30 @@ class Export extends Component {
     });
   }
 
-  render() {
-    const { month, days, dayArr } = this.state;
+  renderContent() {
+    const { showDownload, dayArr } = this.state;
+
+    if (showDownload) {
+      return (
+        <ExcelFile>
+          <ExcelSheet data={dayArr} name={months[month - 1]}>
+            <ExcelColumn label="Total" value="total" />
+            <ExcelColumn label="Gas" value="gas" />
+            <ExcelColumn label="Lotto" value="lotto" />
+            <ExcelColumn label="Inside" value="inside" />
+            <ExcelColumn label="Beer" value="beer" />
+            <ExcelColumn label="Cigarette" value="cigarette" />
+            <ExcelColumn label="Remaining" value="remaining" />
+            <ExcelColumn label="Soda" value="soda" />
+            <ExcelColumn label="Inside W Decimal" value="insideWDecimal" />
+            <ExcelColumn label="Lotto 50" value="lotto50" />
+          </ExcelSheet>
+        </ExcelFile>
+      );
+    }
 
     return (
-      <section id="export-excel">
-        <h1>PMI Tax Helper</h1>
+      <div>
         <select onChange={this.handleSelect} value={month}>
           {
             months.map((month, i) =>
@@ -127,6 +151,18 @@ class Export extends Component {
             }
           </tbody>
         </table>
+        <button onClick={() => this.setState({ showDownload: true })}>Done</button>
+      </div>
+    )
+  }
+
+  render() {
+    const { month, days, dayArr } = this.state;
+
+    return (
+      <section id="export-excel">
+        <h1>PMI Tax Helper</h1>
+        { this.renderContent() }
       </section>
     );
   }
